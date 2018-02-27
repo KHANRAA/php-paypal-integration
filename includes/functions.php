fde6c303987f227b0340111c9b3b4edcf5aaf18d
Faisal Alam <faisal.11616526@lpu.in>
2018-02-28 03:29:12 +0530

    Added function to fetch transaction data.

--- includes/functions.php
+++ includes/functions.php
@@ -18,3 +18,22 @@ function isAssoc(array $arr) {
     if (array() === $arr) return false;
     return array_keys($arr) !== range(0, count($arr) - 1);
 }
+
+function fetchTransactionDetail($token) {
+    $db = Database::connect();
+    $query = "SELECT paypal_txn_id, payment_amount, payment_currency, payment_status, payer_email FROM payments WHERE tracking_id = ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+    $stmt->bind_result($txnId, $amount, $curr, $status, $email);
+    $stmt->fetch();
+    Database::disconnect();
+
+    return array(
+        'transaction_id' => $txnId,
+        'payment_amount' => $amount,
+        'payment_currency' => $curr,
+        'payment_status' => $status,
+        'payer_email' => $email
+    );
+}
