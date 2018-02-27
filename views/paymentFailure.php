bcf793d3aa6029b81d1d07baf1b5aaf48ef2032b
Faisal Alam <faisal.11616526@lpu.in>
2018-02-28 03:26:25 +0530

    Display payment failure message.

--- views/paymentFailure.php
+++ views/paymentFailure.php
@@ -5,3 +5,14 @@
  * Date: 27-02-2018
  * Time: 02:37
  */
+
+// TODO: Design the page to look good.
+
+if (isset($_SESSION['paypal']['token'])) {
+    $paypalClient = new \Paypal\Paypal();
+    $paypalClient->unsetSessionVars();
+
+    echo "Payment was failed!";
+} else {
+    // Invalid request. Redirect the user to the homepage (or anywhere you'd like to).
+}
