9d7d5d19533f146ae5138f26844b4200cba303c2
Faisal Alam <faisal.11616526@lpu.in>
2018-02-28 03:18:58 +0530

    Configured to receive request from IPN.

--- views/paymentCnf.php
+++ views/paymentCnf.php
@@ -5,3 +5,24 @@
  * Date: 27-02-2018
  * Time: 02:36
  */
+
+$paypalClient = new \Paypal\Paypal();
+$paypalClient->readIPNResponse();
+$paypalClient->buildVerificationQuery();
+
+try {
+    $status = $paypalClient->validatePayment();
+} catch(Exception $e) {
+    // TODO: Handle failure.
+}
+
+if ($status === TRUE) {
+    try {
+        $paypalClient->updatePaymentStatus();
+    } catch (Exception $e) {
+        // Read tracking ID from $_POST['custom'] and log a request against this ID to notify the admin.
+    }
+} else {
+    // When an invalid response is received.
+    header('Location: .');
+}
