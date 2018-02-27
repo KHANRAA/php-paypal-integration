71f9961c2accc6b5bde68494cadf144aa809a589
Faisal Alam <faisal.11616526@lpu.in>
2018-02-28 03:25:50 +0530

    Display payment success data.

--- views/paymentSuccess.php
+++ views/paymentSuccess.php
@@ -5,3 +5,18 @@
  * Date: 27-02-2018
  * Time: 02:37
  */
+
+// TODO: Design the page to look good.
+
+if (isset($_SESSION['paypal']['token'])) {
+    $paypalClient = new \Paypal\Paypal();
+
+    $token = $_SESSION['paypal']['token'];
+    $details = fetchTransactionDetail($token);
+
+    print_r($details);
+
+    $paypalClient->unsetSessionVars();
+} else {
+    // Invalid request. Redirect the user to the homepage (or anywhere you'd like to).
+}
